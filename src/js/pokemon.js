7296d40d72fb0f4110f04df437c0f384347f7771
Alvaro Isorna <isorna@users.noreply.github.com>
2025-03-19 15:31:04 +0100

    feat(pokedex): lectura de datos

--- src/js/pokemon.js
+++ src/js/pokemon.js
@@ -1,3 +1,6 @@
+// 3. READ base de datos
+import pokedex from '/pokedex/pokedex.json' with { type: "json" }
+
 /*
 # Tarea: buscar un pokemon
 
@@ -7,87 +10,39 @@
 4. Devuelvo los datos de ese pokemon a la página web
 5. Limpio la tabla de pokemons para dejarla preparada
 6. Añado un LI a la lista ordenada de pokemons con los datos del pokemon y la estructura HTML
-
-## Pseudocódigo
-
-1. SET busqueda = INPUT nombre o número de pokemon
-2. CLICK en botón de submit
-3. READ base de datos
-4. FOR cada pokemon de la base de datos
-4.1. IF encuentro pokemon RETURN datos del pokemon
-4.2. ELSE devuelvo "pokemon no encontrado"
-5. SHOW tabla de datos (lista-pokemons) limpia
-6.1. IF hay pokemon, lo añado a la lista con DISPLAY
-6.1.1. IF hay más de un pokemon, con FOR por cada pokemon añado su ficha a la lista
-6.2. ELSE no hay pokemon, muestro "pokemon no encontrado" en lugar de la lista-pokemons
 */
 
-// Variable normal
-let variable_1 = 'variable 1'
+window.addEventListener("DOMContentLoaded", onDOMContentLoaded)
 
-// Ejemplo de Template literal
-let comida = 'fruta'
-let nombrePokemon = 'Pikachu tiene hambre, voy a darle de comer ' + comida
+/**
+ * Evento que se lanza cuando el contenido de la página ha sido cargado en memoria
+ * y se puede acceder a él.
+ * @listens DOMContentLoaded
+ */
+function onDOMContentLoaded() {
+  let botonBuscar = document.getElementById('botonBuscar')
+  console.log('Ya está disponible la página')
 
-nombrePokemon = `Pikachu tiene hambre,
-voy a darle de comer`
-console.log(1, nombrePokemon);
-
-nombrePokemon = `Pikachu tiene hambre,
-voy a darle de comer ${comida}`
-console.log(2, nombrePokemon);
-
-comida = 'pescado'
-nombrePokemon = `Pikachu tiene hambre,
-voy a darle de comer ${comida}`
-console.log(3, nombrePokemon);
-
-let variable3 = 1345
-let variableLista = [
-  'variable 1',
-  'variable 2',
-  'variable 3',
-  144
-]
-// camelCase
-let miPokemon = {
-  nombre: 'Bulbasaur',
-  imagen: './img/120x120.svg',
-  numero: '0001',
-  tipos: ['Planta', 'Veneno']
+  // Asigno los eventos que se observan a partir de que cargue la página
+  // 2. CLICK en botón de submit
+  botonBuscar.addEventListener('click', buscarPokemon)
 }
 
-// CONSTANTE
-const MI_CONSTANTE = 'mi constante'
-const IDIOMAS = [
-  'es',
-  'en',
-  'fr',
-  'de'
-]
+/**
+ * Busco un pokemon determinado usando el formulario de búsqueda
+ */
+function buscarPokemon() {
+  // 1. SET busqueda = INPUT nombre o número de pokemon
+  let campoBusqueda = document.getElementById('busqueda')
 
-IDIOMAS.push('it')
+  // 4. FOR cada pokemon de la base de datos
+  // 4.1. IF encuentro pokemon RETURN datos del pokemon
+  // 4.2. ELSE devuelvo "pokemon no encontrado"
+  // 5. SHOW tabla de datos (lista-pokemons) limpia
+  // 6.1. IF hay pokemon, lo añado a la lista con DISPLAY
+  // 6.1.1. IF hay más de un pokemon, con FOR por cada pokemon añado su ficha a la lista
+  // 6.2. ELSE no hay pokemon, muestro "pokemon no encontrado" en lugar de la lista-pokemons
 
-IDIOMAS.forEach((idioma) => {
-  console.log('IDIOMAS.forEach', idioma)
-})
-
-// kebab-case (NO SE USA EN JAVASCRIPT)
-// snake_case (SE USA EN PHP y PYTHON)
-
-// var: No la usaremos
-var variable2 = 'variable 2'
-
-// caso práctico: añadir pokemons a la lista
-const LISTA_POKEMONS = []
-
-function addPokemon() {
-  let nuevoPokemon = document.getElementById('nuevoPokemon').value
-  LISTA_POKEMONS.push(nuevoPokemon)
-  console.log('LISTA_POKEMONS', LISTA_POKEMONS)
+  console.log('Estoy buscando', campoBusqueda.value)
+  console.log('buscando pokemon... He encontrado ', pokedex)
 }
-
-console.log('on load pokemon.js');
-console.log('variables', variable_1, variable2, variable3);
-console.log('objetos y arrays', miPokemon, variableLista);
-console.log('constantes', MI_CONSTANTE, IDIOMAS);
