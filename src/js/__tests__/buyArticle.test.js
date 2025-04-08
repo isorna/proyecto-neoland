5e72c7f0a1baa35a09e5af95fa08d8e9868da4ac
Alvaro Isorna <isorna@users.noreply.github.com>
2025-04-08 09:28:11 +0200

    feat(testing): tests básicos

--- src/js/__tests__/buyArticle.test.js
+++ src/js/__tests__/buyArticle.test.js
@@ -1,9 +1,13 @@
 /* eslint-disable no-undef */
-import { testBuyArticle } from './testBuyArticle.js'
+import { testBuyArticle } from '../lib/testBuyArticle.js'
 
 describe('Voy a probar que se realiza la suma correctamente', () => {
   test('sumar 1 + 2 es igual a 3', () => {
     // 1 assertion
     expect(testBuyArticle(1, 2)).toBe(3);
+  });
+  test('sumar 1 + 2 es igual a 4', () => {
+    // 1 assertion
+    expect(testBuyArticle(1, 2)).toBe(4);
   });
 })
