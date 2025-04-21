49c2aa2ec32dc2a532f23f22c89b8167d6b25572
Alvaro Isorna <isorna@users.noreply.github.com>
2025-04-21 15:04:54 +0200

    feat(mongodb): librerias de express y mongodb

---
+++ server/server.simple.express.js
@@ -0,0 +1,37 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import { crud } from "./server.crud.js";
+
+const app = express();
+const port = process.env.PORT;
+const USERS_URL = './server/BBDD/users.json'
+
+// Static server
+app.use(express.static('src'));
+// for parsing application/json
+app.use(bodyParser.json())
+
+// Router de la aplicación
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+
+app.get('/read/users', (req, res) => {
+  crud.read(USERS_URL, (data) => {
+    console.log('server read users', data)
+
+    res.send(JSON.stringify(data));
+  });
+});
+
+app.post('/create/users', (req, res) => {
+  crud.create(USERS_URL, req.body, (data) => {
+    console.log(`server create user ${data.name} creado`, data)
+    res.send(JSON.stringify(data));
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+})
