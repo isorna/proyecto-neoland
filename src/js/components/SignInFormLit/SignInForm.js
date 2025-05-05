a116b3f79eed8caa1107148e7407ed60567d7b5c
Alvaro Isorna <isorna@users.noreply.github.com>
2025-05-05 11:34:57 +0200

    feat(web components): asignación de eventos con lit element

--- src/js/components/SignInFormLit/SignInForm.js
+++ src/js/components/SignInFormLit/SignInForm.js
@@ -1,5 +1,6 @@
 // Si no usamos el Starter Kit:
 import { LitElement, html } from 'https://cdn.jsdelivr.net/gh/lit/dist@3/all/lit-all.min.js';
+import { getAPIData, getInputValue, API_PORT } from '../../index.js';
 // Si usamos el Starter Kit:
 // import { LitElement, html } from 'lit';
 // @ ts-expect-error TS doesn't like this
@@ -14,7 +15,7 @@ export class SignInFormLit extends LitElement {
 
   render() {
     return html`
-      <form id="signInForm">
+      <form id="signInForm" @submit="${this._onFormSubmit}">
         <slot></slot>
         <p id="infoMessage">Registro del usuario</p>
         <input type="text" id="name" placeholder="Nombre de usuario" required>
@@ -22,6 +23,35 @@ export class SignInFormLit extends LitElement {
         <button type="submit">Sign In</button>
       </form>
     `;
+  }
+
+  // Private Methods
+  async _onFormSubmit(event) {
+    event.preventDefault();
+    const name = this.renderRoot.getElementById("name");
+    const email = this.renderRoot.getElementById("email");
+    const signInData = {
+      name: getInputValue(name),
+      email: getInputValue(email)
+    }
+
+    console.log(`DESDE DENTRO DEL COMPONENTE Name: ${signInData.name}, Email: ${signInData.email}`);
+
+    if (signInData.email !== '' && signInData.password !== '') {
+      const payload = JSON.stringify(signInData)
+      const apiData = await getAPIData(`${location.protocol}//${location.hostname}${API_PORT}/api/login`, 'POST', payload)
+      console.log(apiData)
+      // onFormSubmitEvent = new CustomEvent("login-form-submit", {
+      //   bubbles: true,
+      //   detail: apiData
+      // })
+    } else {
+      console.error('No hay datos')
+      // onFormSubmitEvent = new CustomEvent("login-form-submit", {
+      //   bubbles: true,
+      //   detail: null
+      // })
+    }
   }
 }
 customElements.define('signin-form-lit', SignInFormLit);
